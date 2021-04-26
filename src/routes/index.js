aaae43c3c146e00d3042b51d58299f0401248593
Valentina <shingaki@hotmail.com.ar>
2021-04-26 20:03:39 -0300

    add structure

---
+++ src/routes/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const path = require("path");
+const fs = require('fs');
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  res.render("index", {title:"Página principal"});
+});
+
+router.get("/formulario", (req, res) => {
+  res.render("formulario", {title:"formulario",});
+});
+
